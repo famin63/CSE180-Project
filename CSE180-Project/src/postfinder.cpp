00a59e8126d010b1280c7e96687c144136559755
famin63 <87962678+famin63@users.noreply.github.com>
2023-05-01 16:53:53 -0700

    Update postfinder.cpp

--- CSE180-Project/src/postfinder.cpp
+++ CSE180-Project/src/postfinder.cpp
@@ -14,137 +14,130 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// #include <rclcpp/rclcpp.hpp>
-// #include <navigation/navigation.hpp>
-// #include <sensor_msgs/msg/laser_scan.hpp>
-// #include <geometry_msgs/msg/pose_stamped.hpp>
-// #include <nav_msgs/msg/occupancy_grid.hpp>
-// #include <tf2_ros/transform_listener.h>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-// void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan_msg, Navigator& navigator, nav_msgs::msg::OccupancyGrid& map) {
-//   // Retrieve the transform of the laser with respect to the base_link
-//   tf2_ros::Buffer tf_buffer;
-//   tf2_ros::TransformListener tf_listener(tf_buffer);
-//   geometry_msgs::msg::TransformStamped transformStamped;
-//   try {
-//     transformStamped = tf_buffer.lookupTransform("base_link", scan_msg->header.frame_id, scan_msg->header.stamp, rclcpp::Duration(1.0));
-//   } catch (tf2::TransformException& ex) {
-//     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "%s", ex.what());
-//     return;
-//   }
-
-//   // Compute the position of the laser in the map frame
-//   geometry_msgs::msg::PoseStamped laser_pose;
-//   laser_pose.pose.position.x = transformStamped.transform.translation.x;
-//   laser_pose.pose.position.y = transformStamped.transform.translation.y;
-//   laser_pose.pose.orientation = transformStamped.transform.rotation;
-//   laser_pose.header.frame_id = "base_link";
-//   laser_pose.header.stamp = scan_msg->header.stamp;
-
-//   // Transform the laser pose to the map frame
-//   geometry_msgs::msg::PoseStamped map_pose;
-//   try {
-//     tf_buffer.transform(laser_pose, map_pose, "map", rclcpp::Duration(1.0));
-//   } catch (tf2::TransformException& ex) {
-//     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "%s", ex.what());
-//     return;
-//   }
-
-//   // Compute the map cell corresponding to the laser pose
-//   int x = (int)((map_pose.pose.position.x - map.info.origin.position.x) / map.info.resolution);
-//   int y = (int)((map_pose.pose.position.y - map.info.origin.position.y) / map.info.resolution);
-
-//   // Check if the map cell is occupied or free
-//   if (x >= 0 && x < map.info.width && y >= 0 && y < map.info.height) {
-//     int index = y * map.info.width + x;
-//     if (map.data[index] > 65) {
-//       RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Laser scan reports occupied space at (%d,%d) but map says it is free", x, y);
-//     } else if (map.data[index] < 20) {
-//       RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Laser scan reports free space at (%d,%d) but map says it is occupied", x, y);
-//     }
-//   }
-// }
 #include <rclcpp/rclcpp.hpp>
 #include <navigation/navigation.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <iostream>
 
-void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan_msg, Navigator& navigator, nav_msgs::msg::OccupancyGrid& map, const geometry_msgs::msg::PoseStamped::SharedPtr amcl_pose) {
-  // Retrieve the transform of the laser with respect to the base_link
-  static tf2_ros::Buffer tf_buffer;
-  static tf2_ros::TransformListener tf_listener(tf_buffer);
+using namespace std;
 
-  geometry_msgs::msg::TransformStamped transformStamped;
-  try {
-    transformStamped = tf_buffer.lookupTransform("base_link", scan_msg->header.frame_id, scan_msg->header.stamp, rclcpp::Duration(1.0));
-  } catch (tf2::TransformException& ex) {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "%s", ex.what());
-    return;
-  }
+// global variables
+nav_msgs::msg::OccupancyGrid map_data;
+geometry_msgs::msg::Pose amcl_pose;
+bool amcl_pose_received = false;
+bool map_received = false;
+bool laser_received = false;
+geometry_msgs::msg::TransformStamped laser_tf;
 
-  // Compute the position of the laser in the map frame
-  geometry_msgs::msg::PoseStamped laser_pose;
-  laser_pose.pose.position.x = transformStamped.transform.translation.x;
-  laser_pose.pose.position.y = transformStamped.transform.translation.y;
-  laser_pose.pose.orientation = transformStamped.transform.rotation;
-  laser_pose.header.frame_id = "base_link";
-  laser_pose.header.stamp = scan_msg->header.stamp;
+// callback function for the laser scan data
+void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan_data) {
+  laser_received = true;
+}
 
-  // Transform the laser pose to the map frame using the amcl_pose
-  geometry_msgs::msg::PoseStamped map_pose;
-  try {
-    geometry_msgs::msg::TransformStamped amcl_transform = tf_buffer.lookupTransform("map", amcl_pose->header.frame_id, amcl_pose->header.stamp, rclcpp::Duration(1.0));
-    tf2::doTransform(amcl_pose->pose, map_pose.pose, amcl_transform);
-    map_pose.header = amcl_pose->header;
-  } catch (tf2::TransformException& ex) {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "%s", ex.what());
-    return;
-  }
+// callback function for the occupancy grid data
+void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map) {
+  map_data = *map;
+  map_received = true;
+}
 
-  // Compute the map cell corresponding to the laser pose
-  int x = (int)((map_pose.pose.position.x - map.info.origin.position.x) / map.info.resolution);
-  int y = (int)((map_pose.pose.position.y - map.info.origin.position.y) / map.info.resolution);
-
-  // Check if the map cell is occupied or free
-  if (x >= 0 && x < map.info.width && y >= 0 && y < map.info.height) {
-    int index = y * map.info.width + x;
-    if (map.data[index] > 65) {
-      RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Laser scan reports occupied space at (%d,%d) but map says it is free", x, y);
-    } else if (map.data[index] < 20) {
-      RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Laser scan reports free space at (%d,%d) but map says it is occupied", x, y);
-    }
-  }
+// callback function for the AMCL pose
+void amclPoseCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr pose) {
+  amcl_pose = pose->pose.pose;
+  amcl_pose_received = true;
 }
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv); // initialize ROS
   Navigator navigator(true, false); // create node with debug info but not verbose
 
-  // Subscribe to the map
-  auto map_sub = navigator.create_subscription<nav_msgs::msg::OccupancyGrid>(
-    "/map", 10,
-    [&](const nav_msgs::msg::OccupancyGrid::SharedPtr map_msg) {
-      // Store the map
-      navigator.SetMap(map_msg);
-    });
+  // first: it is mandatory to initialize the pose of the robot
+  geometry_msgs::msg::Pose::SharedPtr init = std::make_shared<geometry_msgs::msg::Pose>();
+  init->position.x = -2;
+  init->position.y = -0.5;
+  init->orientation.w = 1;
+  navigator.SetInitialPose(init);
 
-  // Subscribe to the laser scan
+  // wait for navigation stack to become operational
+  navigator.WaitUntilNav2Active();
+
+  // subscribe to the laser scan data
   auto laser_sub = navigator.create_subscription<sensor_msgs::msg::LaserScan>(
-    "/scan", 10,
-    [&](const sensor_msgs::msg::LaserScan::SharedPtr scan_msg) {
-      // Call laser callback function
-      laserCallback(scan_msg, navigator, navigator.GetMap(), amcl_pose);
-    });
+    "/scan", 10, laserCallback);
 
-  // Spin the node
-  rclcpp::spin(navigator.get_node_base_interface());
+  // subscribe to the occupancy grid data
+  auto map_sub = navigator.create_subscription<nav_msgs::msg::OccupancyGrid>(
+    "/map", 10, mapCallback);
 
-  // Shut down ROS
-  rclcpp::shutdown();
+  // subscribe to the AMCL pose
+  auto amcl_pose_sub = navigator.create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+    "/amcl_pose", 10, amclPoseCallback);
 
-  return 0;
+  // create a transform listener for the laser scan tf
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf_listener(tf_buffer);
+
+  // move to initial position
+  while (!navigator.IsTaskComplete()) {
+    // busy waiting for task to be completed
+  }
+
+  // backup of 0.15 m (default distance)
+  navigator.Backup();
+  while (!navigator.IsTaskComplete()) {
+    // busy waiting for task to be completed
+  }
+
+  // patrol the map
+  while (rclcpp::ok()) {
+    if (!amcl_pose_received || !map_received || !laser_received) {
+      // wait for all the required data to be received
+      rclcpp::spin_some(navigator.get_node_base_interface());
+      continue;
+    }
+
+    // get the laser scan tf
+    try {
+      laser_tf = tf_buffer.lookupTransform("base_link", "laser", tf2::TimePoint());
+    }
+    catch (tf2::TransformException &ex) {
+      RCLCPP_WARN(navigator.get_logger(), "Failed to get laser scan tf: %s", ex.what());
+      continue;
+    }
+
+    // loop over all the cells in the map
+    for (int i = 0; i < map_data.info.width * map_data.info.height; i++) {
+// convert cell index to coordinates
+int x = i % map_data.info.width;
+int y = i / map_data.info.width;
+double map_x = (x - (double)map_data.info.width/2) * map_data.info.resolution;
+double map_y = (y - (double)map_data.info.height/2) * map_data.info.resolution;
+  // check if the cell is occupied in the map
+  if (map_data.data[i] > 65) {
+    // calculate distance from robot to cell
+    double dist = sqrt(pow(amcl_pose.position.x - map_x, 2) + pow(amcl_pose.position.y - map_y, 2));
+
+    // get range measurement from laser scan data
+    double angle = atan2(map_y - amcl_pose.position.y, map_x - amcl_pose.position.x) - tf2::getYaw(amcl_pose.orientation);
+    double range = scan_data->ranges[angle / scan_data->angle_increment];
+
+    // check if there is a mismatch between the map and the laser data
+    if (range < dist - 0.1) {
+      // log the mismatch
+      RCLCPP_INFO(navigator.get_logger(), "Mismatch found: Map says occupied at (%f, %f), but laser data indicates free space.", map_x, map_y);
+    }
+  }
+}
+
+// wait for next laser scan data
+laser_received = false;
+while (!laser_received && rclcpp::ok()) {
+  rclcpp::spin_some(navigator.get_node_base_interface());
+}
+}
+
+rclcpp::shutdown(); // shutdown ROS
+return 0;
 }
