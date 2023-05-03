ebd72843741b5ef7b74a8de819c26cac13c4dc4d
famin63 <87962678+famin63@users.noreply.github.com>
2023-05-03 08:37:36 -0700

    Update postfinder.cpp

--- CSE180-Project/src/postfinder.cpp
+++ CSE180-Project/src/postfinder.cpp
@@ -22,63 +22,24 @@ limitations under the License.
 
 
 
-// void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg){
-//         RCLCPP_INFO(nodeh->get_logger(),"Size of Map: %d", msg->data.size());
-//         RCLCPP_INFO(nodeh->get_logger(), "Resolution: %f", msg->info.resolution());
-//         RCLCPP_INFO(nodeh->get_logger(), "Width: %d", msg->info.width());
-//         RCLCPP_INFO(nodeh->get_logger(), "Height: %d", msg->info.height());
+void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg){
+        RCLCPP_INFO(nodeh->get_logger(),"Size of Map: %d", msg->data.size());
+        RCLCPP_INFO(nodeh->get_logger(), "Resolution: %f", msg->info.resolution());
+        RCLCPP_INFO(nodeh->get_logger(), "Width: %d", msg->info.width());
+        RCLCPP_INFO(nodeh->get_logger(), "Height: %d", msg->info.height());
     
-//         uint counter = 0;
-//         for (uint i = 0; i < msg->data.size(); i++) {
-//             if (-1 != msg->data[i]) {
-//                 counter++;
-//             }
-//         }
-// }
+        uint counter = 0;
+        for (uint i = 0; i < msg->data.size(); i++) {
+            if (-1 != msg->data[i]) {
+                counter++;
+            }
+        }
+}
 
 int main(int argc,char **argv) {
  
   rclcpp::init(argc,argv); // initialize ROS 
-  Navigator navigator(true,false); // create node with debug info but not verbose
-
-  // first: it is mandatory to initialize the pose of the robot
-  geometry_msgs::msg::Pose::SharedPtr init = std::make_shared<geometry_msgs::msg::Pose>();
-  init->position.x = -2;
-  init->position.y = -0.5;
-  init->orientation.w = 1;
-  navigator.SetInitialPose(init);
-  // wait for navigation stack to become operationale
-  navigator.WaitUntilNav2Active();
-  // spin in place of 90 degrees (default parameter)
-  navigator.Spin();
-  while ( ! navigator.IsTaskComplete() ) {
-    // busy waiting for task to be completed
-  }
-  geometry_msgs::msg::Pose::SharedPtr goal_pos = std::make_shared<geometry_msgs::msg::Pose>();
-  goal_pos->position.x = 2;
-  goal_pos->position.y = 1;
-  goal_pos->orientation.w = 1;
-  // move to new pose
-  navigator.GoToPose(goal_pos);
-  while ( ! navigator.IsTaskComplete() ) {
-    
-  }
-  goal_pos->position.x = 2;
-  goal_pos->position.y = -1;
-  goal_pos->orientation.w = 1;
-  navigator.GoToPose(goal_pos);
-  // move to new pose
-  while ( ! navigator.IsTaskComplete() ) {
-    
-  }
-  // backup of 0.15 m (deafult distance)
-  navigator.Backup();
-  while ( ! navigator.IsTaskComplete() ) {
-    
-  }
-
-  // complete here....
-  
-  rclcpp::shutdown(); // shutdown ROS
-  return 0;
+  nodeh = rclcpp::Node::make_shared("map_parser");
+  auto sub = nodeh->create_subscription<nav_msgs::msg::OccupancyGrid>
+       ("global_costmap/costmap", 1000, &mapCallback)
 }
