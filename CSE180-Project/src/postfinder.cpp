d7b47bba657ae0d13da4c691c89824a011bc853c
famin63 <87962678+famin63@users.noreply.github.com>
2023-05-03 08:53:44 -0700

    Update postfinder.cpp

--- CSE180-Project/src/postfinder.cpp
+++ CSE180-Project/src/postfinder.cpp
@@ -20,20 +20,33 @@ limitations under the License.
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <std_msgs/msg/float32.hpp>
 
+nav_msgs::msg::OccupancyGrid::SharedPtr original_map;
+bool original = true;
 
+rclcpp::Node::SharedPtr nodeh;
 
 void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg){
-        RCLCPP_INFO(nodeh->get_logger(),"Size of Map: %d", msg->data.size());
-        RCLCPP_INFO(nodeh->get_logger(), "Resolution: %f", msg->info.resolution());
-        RCLCPP_INFO(nodeh->get_logger(), "Width: %d", msg->info.width());
-        RCLCPP_INFO(nodeh->get_logger(), "Height: %d", msg->info.height());
+        std::vector<int> update_index;
+        int filter = 95;
     
-        uint counter = 0;
-        for (uint i = 0; i < msg->data.size(); i++) {
-            if (-1 != msg->data[i]) {
-                counter++;
+        if(original) {
+            original_map = msg;
+            original = false;
+        }
+        else {
+            for(uint i = 0; i < msg->data.size(); i++){
+                if((int)msg->data[i] - (int)original_map->data[i] > filter){
+                    update_index.push_back(i)
+                }
             }
         }
+    
+//         uint counter = 0;
+//         for (uint i = 0; i < msg->data.size(); i++) {
+//             if (-1 != msg->data[i]) {
+//                 counter++;
+//             }
+//         }
 }
 
 int main(int argc,char **argv) {
